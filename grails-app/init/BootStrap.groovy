710ad7f91e3635e3296018dfa53a48d2f8002f23
Graeme Rocher <graemerocher@Graemes-iMac.local>
2015-02-03 12:35:11 +0100

    functional test for BootStrap classes

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,10 @@
+import functionaltests.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Book(title:"Example Book").save(flush:true)
+        assert Book.count() == 1
     }
     def destroy = {
     }
--- src/test/groovy/functionaltests/BookFunctionalSpec.groovy
+++ src/test/groovy/functionaltests/BookFunctionalSpec.groovy
@@ -10,7 +10,7 @@ import geb.spock.*
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @Integration
-class HomeSpec extends GebSpec {
+class BookFunctionalSpec extends GebSpec {
 
     def setup() {
     }
@@ -18,15 +18,11 @@ class HomeSpec extends GebSpec {
     def cleanup() {
     }
 
-    void "Test the home page renders correctly"() {
+    void "test something"() {
         when:"The home page is visited"
             go '/'
 
         then:"The title is correct"
         	$('title').text() == "Welcome to Grails"
-          $('li.controller',0).text() == 'functionaltests.BookController'
-          // SimpleController should not become a controller as it is not in a grails-app/controllers dir
-          !$('li.controller',1).text()
-
     }
 }
