a4e32cfbc83c8af93b7236f5650787797e90239d
graemerocher <graeme.rocher@gmail.com>
2015-03-04 12:33:52 +0100

    fix failing test

--- app1/src/integration-test/groovy/functionaltests/HomeSpec.groovy
+++ app1/src/integration-test/groovy/functionaltests/HomeSpec.groovy
@@ -27,11 +27,12 @@ class HomeSpec extends GebSpec {
         then:"The title is correct"
         	$('title').text() == "Welcome to Grails"
           $('li.controller',0).text() == 'functionaltests.BookController'
-          $('li.controller',1).text() == 'functionaltests.InspectConfigController'
-          $('li.controller',2).text() == 'functionaltests.MiscController'
-          $('li.controller',3).text() == 'functionaltests.UploadController'
+          $('li.controller',1).text() == 'functionaltests.ErrorsController'
+          $('li.controller',2).text() == 'functionaltests.InspectConfigController'
+          $('li.controller',3).text() == 'functionaltests.MiscController'
+          $('li.controller',4).text() == 'functionaltests.UploadController'
           // SimpleController should not become a controller as it is not in a grails-app/controllers dir
-          !$('li.controller',4).text()
+          !$('li.controller',5).text()
 
     }
 }
