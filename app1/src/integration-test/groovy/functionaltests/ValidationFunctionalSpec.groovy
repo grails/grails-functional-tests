110ee8b9455be28e1acc1b09abacf03d235f714b
Jeff Scott Brown <jbrown@pivotal.io>
2015-03-05 10:46:07 -0600

    GRAILS-12039 - test hibernate entity validation

---
+++ app1/src/integration-test/groovy/functionaltests/ValidationFunctionalSpec.groovy
@@ -0,0 +1,28 @@
+package functionaltests
+
+import grails.test.mixin.integration.Integration
+
+import spock.lang.*
+import geb.spock.*
+
+@Integration
+class ValidationFunctionalSpec extends GebSpec {
+
+    @Issue('GRAILS-12039')
+    void "Test validation of an invalid Hibernate entity"() {
+        when:
+        go '/book/validateBook'
+
+        then:
+        $().text() == 'The validate method returned false'
+    }
+
+    @Issue('GRAILS-12039')
+    void "Test validation of a valid Hibernate entity"() {
+        when:
+        go '/book/validateBook?title=DGG'
+
+        then:
+        $().text() == 'The validate method returned true'
+    }
+}
