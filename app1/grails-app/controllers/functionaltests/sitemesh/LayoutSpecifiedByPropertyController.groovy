9e16b1cb92cf7f7e580b51b04b6acc528e468115
Jeff Scott Brown <jbrown@pivotal.io>
2015-03-16 18:33:32 -0500

    GRAILS-12045 - more sitemesh tests
    This tests the scenario where a sitemesh layout is applied to a page that does not contain a root HTML element.

--- app1/grails-app/controllers/functionaltests/sitemesh/LayoutSpecifiedByPropertyController.groovy
+++ app1/grails-app/controllers/functionaltests/sitemesh/LayoutSpecifiedByPropertyController.groovy
@@ -5,4 +5,8 @@ class LayoutSpecifiedByPropertyController {
     static layout = 'foo'
 
     def index() {}
+
+    def snippetView() {
+        // renders a view that does not contain a root html element
+    }
 }
