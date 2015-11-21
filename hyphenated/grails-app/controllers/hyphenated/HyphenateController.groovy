a2f38d4269d75e5f43c2a3490ae09858cf3d8486
Burt Beckwith <burt@burtbeckwith.com>
2015-11-21 17:37:43 -0500

    updated controller with extra actions to ensure #9431 is resolved

--- hyphenated/grails-app/controllers/hyphenated/HyphenateController.groovy
+++ hyphenated/grails-app/controllers/hyphenated/HyphenateController.groovy
@@ -2,7 +2,18 @@ package hyphenated
 
 class HyphenateController {
 
-    def longName() { 
+    def longName() {
     	render "good"
     }
+
+    // some extra dummy actions to have enough to trigger issue #9431
+    def longName2() {}
+    def longName3() {}
+    def longName4() {}
+    def longName5() {}
+    def longName6() {}
+    def longName7() {}
+    def longName8() {}
+    def longName9() {}
+    def longName10() {}
 }
