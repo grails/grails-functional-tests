a115c7928b4064e6e03eb6a331f4be5d4dd800d6
Jeff Scott Brown <brownj@ociweb.com>
2016-07-30 12:28:08 -0500

    Add data binding test
    This test is testing that auto timestamp properties are by default
    excluded from mass property binding.  We have unit tests in grails-core
    for this and those tests work on classes that are explicitly marked
    with @Entity in the source code.  This test is covering a problem that
    relates to indetifying a domain class as being a domain class if it is
    used as a command object.  Testing this in a real app (like this one)
    covers that problem.

--- app1/grails-app/domain/functionaltests/Book.groovy
+++ app1/grails-app/domain/functionaltests/Book.groovy
@@ -3,6 +3,8 @@ package functionaltests
 class Book {
   
     String title
+    Date lastUpdated
+    Date dateCreated
 
     static constraints = {
     }
--- app1/src/test/groovy/functionaltests/BookSpec.groovy
+++ app1/src/test/groovy/functionaltests/BookSpec.groovy
@@ -1,5 +1,6 @@
 package functionaltests
 import grails.test.mixin.TestFor
+import spock.lang.Issue
 import spock.lang.Specification
 
 /**
@@ -11,5 +12,22 @@ class BookSpec extends Specification {
     void "Test validating a book"() {
         expect:"The book validates"
             new Book(title:"The Stand").validate()
+    }
+
+    @Issue('grails/grails-core#10079')
+    void 'Test that auto-timestamp properties are excluded from mass property binding'() {
+        given:
+        def book = new Book()
+
+        when:
+        book.properties = [title: 'Some Title',
+                           dateCreated: 'some value',
+                           lastUpdated: 'some value']
+
+        then:
+        !book.hasErrors()
+        book.title == 'Some Title'
+        book.dateCreated == null
+        book.lastUpdated == null
     }
 }
