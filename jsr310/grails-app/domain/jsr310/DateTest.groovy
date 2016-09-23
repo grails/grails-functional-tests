e0dee73400f7192cc7376b7ba0f49049effef12a
James Kleeh <jameskleeh@Jamess-MacBook-Pro.local>
2016-09-23 14:21:32 -0400

    Test subproject is ignored on JDK7

---
+++ jsr310/grails-app/domain/jsr310/DateTest.groovy
@@ -0,0 +1,16 @@
+package jsr310
+
+import java.time.*
+
+class DateTest {
+
+    LocalDateTime localDateTime
+    LocalDate localDate
+    LocalTime localTime
+    OffsetDateTime offsetDateTime
+    OffsetTime offsetTime
+    ZonedDateTime zonedDateTime
+
+    static constraints = {
+    }
+}
---
+++ jsr310/src/test/groovy/jsr310/DateTestSpec.groovy
@@ -0,0 +1,22 @@
+package jsr310
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(DateTest)
+class DateTestSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == true
+    }
+}
