bef43217f5397bf82cd539ee5217923467e01dec
Puneet Behl <behl4485@gmail.com>
2018-06-12 17:00:30 +0530

    grails/grails-core#11004 Added test for loadAfter, overriding Spring Security Core plugin auth page from another plugin

--- app1/grails-app/conf/application.groovy
+++ app1/grails-app/conf/application.groovy
@@ -13,3 +13,20 @@ foo {
     baz = 1
 }
 foo.bax = 2
+
+
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+        [pattern: '/',               access: ['permitAll']],
+        [pattern: '/**',             access: ['permitAll']],
+        [pattern: '/**/**',          access: ['permitAll']],
+        [pattern: '/**/**/**',       access: ['permitAll']],
+        [pattern: '/error',          access: ['permitAll']],
+        [pattern: '/index',          access: ['permitAll']],
+        [pattern: '/index.gsp',      access: ['permitAll']],
+        [pattern: '/shutdown',       access: ['permitAll']],
+        [pattern: '/assets/**',      access: ['permitAll']],
+        [pattern: '/**/js/**',       access: ['permitAll']],
+        [pattern: '/**/css/**',      access: ['permitAll']],
+        [pattern: '/**/images/**',   access: ['permitAll']],
+        [pattern: '/**/favicon.ico', access: ['permitAll']]
+]
---
+++ plugins/loadafter/grails-app/views/login/auth.gsp
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="layout" content="main"/>
+    <title>${pageTitle}</title>
+</head>
+<body>
+<div>
+    <p>My Plugin Login Page</p>
+</div>
+</body>
+</html>
