90cfdb94464c013d27679f7945611b24b839f92e
Iván López <lopezi@objectcomputing.com>
2018-12-13 13:46:57 +0100

    Parse the response body as a String

--- issue-11102/src/integration-test/groovy/issue11102/TestControllerSpec.groovy
+++ issue-11102/src/integration-test/groovy/issue11102/TestControllerSpec.groovy
@@ -10,14 +10,14 @@ class TestControllerSpec extends HttpClientCommonSpec {
 
     void 'can forward a request from a GET to another GET action'() {
         when: 'getting the get1 action'
-        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/get1"))
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/get1"), String)
 
         then: 'it is executed correctly'
         response.status == HttpStatus.OK
         response.body() == 'GET1'
 
         when: 'executing an action with a forward to the other one'
-        HttpResponse<String> response2 = client.toBlocking().exchange(HttpRequest.GET("/get2"))
+        HttpResponse<String> response2 = client.toBlocking().exchange(HttpRequest.GET("/get2"), String)
 
         then: 'the request is forwarded'
         response2.status == HttpStatus.OK
