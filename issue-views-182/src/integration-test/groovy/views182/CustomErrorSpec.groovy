d5fa5669ec584ef866ab3d6480f0776ad2348741
Iván López <lopezi@objectcomputing.com>
2018-12-13 10:16:46 +0100

    Replace grails-datastore-rest-client with Micronaut HTTP Client

--- issue-views-182/src/integration-test/groovy/views182/CustomErrorSpec.groovy
+++ issue-views-182/src/integration-test/groovy/views182/CustomErrorSpec.groovy
@@ -1,23 +1,21 @@
 package views182
 
-import grails.plugins.rest.client.RestBuilder
+import grails.gorm.transactions.Rollback
 import grails.testing.mixin.integration.Integration
-import grails.transaction.Rollback
-import spock.lang.Specification
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 
 @Integration
 @Rollback
-class CustomErrorSpec extends Specification {
+class CustomErrorSpec extends HttpClientCommonSpec {
 
     void 'it is possible to use gson views for handling exception errors'() {
-        given: 'a rest client'
-        def builder = new RestBuilder()
-
         when: 'executing get to custom error'
-        def resp = builder.get("http://localhost:${serverPort}/customError")
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/customError"))
 
         then:
-        resp.status == 500
-        resp.text == '{"message":"My custom exception handler","error":500}'
+        response.status == HttpStatus.INTERNAL_SERVER_ERROR
+        response.body() == '{"message":"My custom exception handler","error":500}'
     }
 }
