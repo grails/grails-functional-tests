4c3b2e79205ac1c14d94e6826d8fa20c3888b7cd
sdelamo <sergio.delamo@softamo.com>
2018-12-14 15:30:34 +0100

    Revert "Ignore tests until spring-security-core is migrated"
    This reverts commit d561833ce75d4253321b128f126ab8d227101322.
    
    Close: https://github.com/grails/grails3-functional-tests/issues/20

--- app1/src/integration-test/groovy/functionaltests/LoadAfterSpec.groovy
+++ app1/src/integration-test/groovy/functionaltests/LoadAfterSpec.groovy
@@ -2,12 +2,10 @@ package functionaltests
 
 import geb.spock.GebSpec
 import grails.testing.mixin.integration.Integration
-import spock.lang.Ignore
 
 @Integration
 class LoadAfterSpec extends GebSpec {
 
-    @Ignore("Ignored until spring-security-core is migrated")
     void "Test override one plugin view from another plugin"() {
         when:
         go '/login/auth'
