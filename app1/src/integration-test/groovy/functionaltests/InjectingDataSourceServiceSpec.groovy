19c34209a06239da49d42e29dd8450490fa7177f
sdelamo <sergio.delamo@softamo.com>
2018-12-19 19:02:45 +0100

    Annotate with Pending Feature

--- app1/src/integration-test/groovy/functionaltests/InjectingDataSourceServiceSpec.groovy
+++ app1/src/integration-test/groovy/functionaltests/InjectingDataSourceServiceSpec.groovy
@@ -1,6 +1,7 @@
 package functionaltests
 
 import grails.testing.mixin.integration.Integration
+import spock.lang.PendingFeature
 import spock.lang.Specification
 
 @Integration(applicationClass = Application)
@@ -8,6 +9,7 @@ class InjectingDataSourceServiceSpec extends Specification {
 
     InjectingDataSourceService injectingDataSourceService
 
+    @PendingFeature(reason = "pending GORM release with https://github.com/grails/grails-data-mapping/commit/da53cc19d29f76f4f016749ac08e7e2f2ae44014")
     def "you can inject DataSource with def dataSource"() {
         expect:
         injectingDataSourceService != null
