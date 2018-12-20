f5a4b923cedd2fd6bd55d0df824a31e3a0274815
sdelamo <sergio.delamo@softamo.com>
2018-12-20 09:51:38 +0100

    Correct test to capture HttpClientResponseException

--- issue-views-182/src/integration-test/groovy/views182/CustomErrorSpec.groovy
+++ issue-views-182/src/integration-test/groovy/views182/CustomErrorSpec.groovy
@@ -2,9 +2,11 @@ package views182
 
 import grails.gorm.transactions.Rollback
 import grails.testing.mixin.integration.Integration
+import io.micronaut.core.type.Argument
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 
 @Integration
 @Rollback
@@ -12,10 +14,11 @@ class CustomErrorSpec extends HttpClientCommonSpec {
 
     void 'it is possible to use gson views for handling exception errors'() {
         when: 'executing get to custom error'
-        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/customError"))
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/customError"), Argument.of(String), Argument.of(String))
 
         then:
-        response.status == HttpStatus.INTERNAL_SERVER_ERROR
-        response.body() == '{"message":"My custom exception handler","error":500}'
+        HttpClientResponseException e = thrown()
+        e.response.status == HttpStatus.INTERNAL_SERVER_ERROR
+        e.response.body() == '{"message":"My custom exception handler","error":500}'
     }
 }
