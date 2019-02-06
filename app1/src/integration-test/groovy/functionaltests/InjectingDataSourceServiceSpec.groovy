2209a5614b3a1ef4264f146ea9e7c47754f1c4a8
graemerocher <graeme.rocher@gmail.com>
2019-02-06 14:47:21 +0100

    Remove @PendingFeature now that GORM upgraded

--- app1/src/integration-test/groovy/functionaltests/InjectingDataSourceServiceSpec.groovy
+++ app1/src/integration-test/groovy/functionaltests/InjectingDataSourceServiceSpec.groovy
@@ -9,7 +9,6 @@ class InjectingDataSourceServiceSpec extends Specification {
 
     InjectingDataSourceService injectingDataSourceService
 
-    @PendingFeature(reason = "pending GORM release with https://github.com/grails/grails-data-mapping/commit/da53cc19d29f76f4f016749ac08e7e2f2ae44014")
     def "you can inject DataSource with def dataSource"() {
         expect:
         injectingDataSourceService != null
