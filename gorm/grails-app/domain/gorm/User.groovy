0625266fbe1de765b86e9ac6e2331e6e6dc3539e
Mattias Reichel <mattias.reichel@gmail.com>
2023-11-11 21:05:45 +0100

    Add quotes to table name 'user' to make it compatible with h2 v2.2.224

--- gorm/grails-app/domain/gorm/User.groovy
+++ gorm/grails-app/domain/gorm/User.groovy
@@ -3,8 +3,10 @@ package gorm
 class User {
 
     String name
-    static belongsTo = [city:City]
 
-    static constraints = {
+    static belongsTo = [city: City]
+
+    static mapping = {
+        table '`user`'
     }
 }
